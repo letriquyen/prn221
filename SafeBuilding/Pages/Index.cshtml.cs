4ff7212110c6e507856ec78f7774558667d102f8
Đăng Trường <dtruong1206@gmail.com>
2023-03-14 18:16:49 +0700

    upload and send email

--- SafeBuilding/Pages/Index.cshtml.cs
+++ SafeBuilding/Pages/Index.cshtml.cs
@@ -4,13 +4,17 @@ using Microsoft.VisualBasic;
 using Repository.Models;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System.Diagnostics.Metrics;
 
 namespace SafeBuilding.Pages
 {
-    [IgnoreAntiforgeryToken(Order = 1001)]
     public class IndexModel : PageModel
     {
-        [BindProperty]
+
         public IEnumerable<Invoice> Invoices { get; set; }
 
         private readonly ILogger<IndexModel> _logger;
@@ -27,7 +31,7 @@ namespace SafeBuilding.Pages
             {
                 return RedirectToPage("Login");
             }
-                return Page();
+            return Page();
         }
 
 
@@ -50,9 +54,9 @@ namespace SafeBuilding.Pages
         //    return Page();
         //}
 
-        public IActionResult OnPostUploadFile(IFormFile file, [FromServices] Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        public IActionResult OnPost(IFormFile file, [FromServices] Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
         {
-            string filename = $"{hostingEnvironment.WebRootPath}\\file\\{file.FileName}";
+            string filename = Path.Combine(hostingEnvironment.WebRootPath, "file", file.FileName);
             using (FileStream fileStream = System.IO.File.Create(filename))
             {
                 file.CopyTo(fileStream);
@@ -64,6 +68,8 @@ namespace SafeBuilding.Pages
 
         private List<Invoice> GetInvoiceList(string fName)
         {
+
+            var Email = new MimeMessage();
             List<Invoice> list = new List<Invoice>();
             var fileName = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\file"}" + "\\" + fName;
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
@@ -73,6 +79,31 @@ namespace SafeBuilding.Pages
                 {
                     while (reader.Read())
                     {
+                        string rent = reader.GetValue(0).ToString();
+                        string water = reader.GetValue(1).ToString();
+                        string electicity = reader.GetValue(2).ToString();
+                        string management = reader.GetValue(3).ToString();
+                        string parking = reader.GetValue(4).ToString();
+                        string email = reader.GetValue(5).ToString();
+                        string mainBody = "<tr>" +
+                                                "<th>" + reader.GetValue(0).ToString() + "</th>"+
+                                                "<th>" + reader.GetValue(1).ToString() + "</th>" +
+                                                "<th>" + reader.GetValue(2).ToString() + "</th>" +
+                                                "<th>" + reader.GetValue(3).ToString() + "</th>" +
+                                                "<th>" + reader.GetValue(4).ToString() + "</th>" +
+                                            "</tr>";
+                        string body = "<html><head></head><body>" +
+                @"<table border=""1"" cellpadding=""2"" style=""border-collapse: collapse;""><tr style=""color:white;background-Color:SkyBlue;font-weight:bold;"">" +
+                "<td>Rent</td><td>Water</td><td>Electricity</td><td>Management</td><td>Parking</td>" + "</tr>" + mainBody + "</table></body></html>";
+                        Email.From.Add(MailboxAddress.Parse("safebuilding76@gmail.com"));
+                        Email.To.Add(MailboxAddress.Parse(email));
+                        Email.Subject = "New Invoice from SafeBuilding";
+                        Email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+                        using var smtp = new SmtpClient();
+                        smtp.Connect("smtp.gmail.com", 465, true);
+                        smtp.Authenticate("safebuilding76@gmail.com", "afcllbwpxmsebrnw");
+                        smtp.Send(Email);
+                        smtp.Disconnect(true);
                         list.Add(new Invoice()
                         {
                             rent = reader.GetValue(0).ToString(),
